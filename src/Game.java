57b2ec03c54bbdbee1b4bbd707720f09c701eca3
alyssathomason <34072898+alyssathomason@users.noreply.github.com>
2018-03-19 10:10:34 -0700

    Changes to files and such

--- src/Game.java
+++ src/Game.java
@@ -4,36 +4,32 @@ import javax.swing.*;
 import javax.swing.plaf.metal.MetalButtonUI;
 
 public class Game extends JFrame implements ActionListener {
+    static final long serialVersionUID = 106664208;
     public boolean inGame;
     final int X = 600, Y = 750;
 
     private JFrame window;
     private JMenuBar mnuMain;
     private JButton startGameButton;
-    private JLabel mainTitle1, mainTitle2, mainTitle3, mainTitle4;
+    private JLabel mainTitle1, mainTitle2, mainTitle3, mainTitle4, lblQuestion;
     private JMenuItem   mnuClearQuiz, mnuGameTitle, mnuStartQuiz, mnuExit;
 
     private JPanel pnlBar, pnlGame, pnlTitle, pnlTitlePicture, pnlTitlePage, pnlQuestion, pnlPicture, pnlAnswer;
 
     private Font fontToken = new Font("Impact", Font.BOLD, 70);
-    private Font fontTitle = new Font("Impact", Font.BOLD, 100);
+    private Font fontTitle = new Font("Impact", Font.BOLD, 15);
     private Font fontMenu = new Font("Impact", Font.BOLD, 18);
 
     private JButton answerChoices[];
     private String[] choices = new String[]{"A", "B", "C", "D"};
     private String[] answerKey;
-
+    private String[] questions = { "Press button for to begin", "This revenger who?",
+            "Conglaturations! A winner is you! Pray againe?", "Haha you loose, now world is will be destroy!" };
     public void init() {
         inGame = false;
 
-        window = new JFrame("|||||||||||||||||||REVENGERS REUNITE|||||||||||||||||||");
-
-        mnuMain = new JMenuBar();
-        mnuGameTitle = new JMenuItem("HOME  ");
-        mnuClearQuiz = new JMenuItem("  CLEAR QUIZ");
-        mnuStartQuiz = new JMenuItem(" START QUIZ");
-        mnuExit = new JMenuItem("    GIVE UP");
-
+        window = new JFrame("REVENGERS REUNITE");
+        
         pnlBar = new JPanel();
         pnlGame = new JPanel();
         pnlTitlePage = new JPanel();
@@ -49,7 +45,13 @@ public class Game extends JFrame implements ActionListener {
         window.setLayout(new BorderLayout());
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //start menu initialization
+        //start MENU initialization
+        mnuMain = new JMenuBar();
+        mnuGameTitle = new JMenuItem("HOME   ");
+        mnuClearQuiz = new JMenuItem("   CLEAR QUIZ");
+        mnuStartQuiz = new JMenuItem("   START QUIZ");
+        mnuExit = new JMenuItem("   GIVE UP");
+        //adding menu items to overall menu
         mnuMain.add(mnuGameTitle);
         mnuGameTitle.setFont(fontMenu);
         mnuMain.add(mnuClearQuiz);
@@ -58,24 +60,19 @@ public class Game extends JFrame implements ActionListener {
         mnuStartQuiz.setFont(fontMenu);
         mnuMain.add(mnuExit);
         mnuExit.setFont(fontMenu);
-        // adding Action Listener to all the Buttons and Menu Items
-        mnuClearQuiz.addActionListener(this);
-        mnuExit.addActionListener(this);
-        mnuClearQuiz.addActionListener(this);
-        //adding menu to panel
-        pnlBar.add(mnuMain);
-        pnlBar.setBackground(new Color(75, 255, 0));
-        //end menu initialization;
         //adding Action Listeners to menu buttons
         mnuClearQuiz.addActionListener(this);
         mnuExit.addActionListener(this);
         mnuStartQuiz.addActionListener(this);
         mnuGameTitle.addActionListener(this);
-
-        //title page test test test
-
-        pnlTitlePage.setLayout(new GridLayout(X, Y - 180, 2, 2));
-        pnlTitlePage.setBackground(new Color(69, 62, 63));
+        //adding menu to panel
+        pnlBar.add(mnuMain);
+        pnlBar.setBackground(new Color(75, 255, 0)); //color = bright green
+        //end menu initialization;
+        
+        //title page
+        pnlTitlePage.setLayout(new BorderLayout());
+        pnlTitlePage.setBackground(new Color(255, 0, 0)); //color = red
         mainTitle1 = new JLabel("wElCOmE");
         mainTitle1.setFont(fontTitle);
         pnlTitle.add(mainTitle1);
@@ -88,14 +85,16 @@ public class Game extends JFrame implements ActionListener {
         mainTitle4 = new JLabel("QuIz");
         mainTitle4.setFont(fontTitle);
         pnlTitle.add(mainTitle4);
-        pnlTitlePage.add(pnlTitle);
+        pnlTitle.setBackground(new Color(255, 0, 0)); //color = red
+        pnlTitlePage.add(pnlTitle, BorderLayout.NORTH);
+        //adding the start game button
         startGameButton = new JButton("Start Game");
         startGameButton.setFont(fontTitle);
         startGameButton.addActionListener(this);
-        pnlTitlePage.add(startGameButton);
-        startGameButton.addActionListener(this);
+        startGameButton.setBackground(new Color(255, 255, 0)); //color = yellow
+        pnlTitlePage.add(startGameButton, BorderLayout.SOUTH);
         startGameButton.setUI(new MetalButtonUI() {
-            protected Color getEnabledTextColor() {
+            protected Color getDisabledTextColor() {
                 return Color.GREEN;
             }
             protected Color getFocusColor() {
@@ -109,89 +108,83 @@ public class Game extends JFrame implements ActionListener {
 
         showTitlePage();
 
-        pnlGame.setLayout(new GridLayout(X, Y - 180, 2, 2));
-        pnlGame.setBackground(new Color(255, 77, 178)); //background behind buttons
-
-        goToTitle();
-
         window.add(pnlBar, BorderLayout.NORTH);
-        window.add(pnlTitlePage, BorderLayout.CENTER);
         window.setVisible(true);
 
         //game board
-
+        pnlGame.setLayout(new BorderLayout());
+        pnlGame.setBackground(new Color(255, 0, 145)); //background of game board; color = pink
+        //question
+        lblQuestion = new JLabel(questions[1]);
+        lblQuestion.setFont(fontMenu);;
+        pnlQuestion.add(lblQuestion);
+        pnlQuestion.setBackground(new Color(160, 0, 255)); //color = purple
+        pnlGame.add(pnlQuestion, BorderLayout.NORTH);
+        //set up picture area
+        pnlGame.add(pnlPicture, BorderLayout.CENTER);
+        pnlPicture.setBackground(new Color(255, 135, 0)); //color = burnt orange
+        //set up answer choices
         pnlAnswer.setLayout(new GridLayout(4, 2, 2, 2));
-        pnlAnswer.setBackground(new Color(0, 255, 244)); //background behind buttons
+        pnlAnswer.setBackground(new Color(0, 255, 245)); //background behind buttons; color = tourquoise
         for(int x = 0; x < 3; x++) {
-            answerChoices[x] = new JButton();
-            answerChoices[x].setText(choices[x]);
-            answerChoices[x].setBackground(new Color(0, 0, 0)); //text color of symbol
+            answerChoices[x] = new JButton(choices[x]);
+            answerChoices[x].setBackground(new Color(0, 247, 255)); //text color of symbol; color = bright blue
             answerChoices[x].addActionListener(this);
             pnlAnswer.add(answerChoices[x]);
             answerChoices[x].setEnabled(true);
             answerChoices[x].setUI(new MetalButtonUI() {
                 protected Color getDisabledTextColor() {
-                    return Color.WHITE;
+                    return Color.BLACK;
                 }
                 protected Color getFocusColor() {
-                    return Color.BLACK;
+                    return Color.WHITE;
                 }
                 protected Color getSelectColor() {
                     return Color.BLACK;
                 }
             });
         }
+        pnlGame.add(pnlAnswer, BorderLayout.SOUTH);
 
     }
     public void actionPerformed(ActionEvent click) {
         // get the mouse click from the user
         Object source = click.getSource();
         if (source == mnuGameTitle) {
-            goToTitle();
+            showTitlePage();
         } else if (source == mnuClearQuiz) {
             clearGameBoard();
             /*startGame();*/
         } else if (source == mnuExit) {
             exitGame();
         } else if (source == mnuStartQuiz) {
-            clearGameBoard();
             showGame();
             /*startGame();*/
         } else if (source == startGameButton) {
-            clearGameBoard();
             showGame();
             // startGame();
         }
     }
     //methods for what the game will look like
     public void showTitlePage() {
-        pnlGame.add(pnlTitlePage);
-        pnlTitlePage.setLayout(new GridLayout(1, 0));
+        clearGameBoard();
+        window.add(pnlTitlePage, BorderLayout.CENTER);
+        startGameButton.setEnabled(true);
+        startGameButton.setVisible(true);
         pnlTitlePage.requestFocus();
-        pnlGame.revalidate();
     }
     public void showGame() {
-        pnlGame.setLayout(new FlowLayout(FlowLayout.CENTER));
-        pnlGame.setBackground(new Color(255, 42, 0));
-        window.add(pnlBar, BorderLayout.NORTH);
+        clearGameBoard();
         window.add(pnlGame, BorderLayout.CENTER);
-        window.setVisible(true);
-        pnlGame.setLayout(new BorderLayout());
-        pnlGame.add(pnlQuestion, BorderLayout.NORTH);
-        pnlGame.add(pnlPicture, BorderLayout.CENTER);
-        pnlGame.add(pnlAnswer, BorderLayout.SOUTH);
-        pnlQuestion.requestFocus();
-        pnlPicture.requestFocus();
-        pnlAnswer.requestFocus();
-        pnlGame.revalidate();
+        pnlGame.requestFocus();
+        window.revalidate();
     }
     private void clearGameBoard() {
-        pnlGame.remove(pnlTitlePage);
+        window.remove(pnlTitlePage);
+        startGameButton.setEnabled(false);
         startGameButton.setVisible(false);
-        pnlGame.remove(pnlQuestion);
-        pnlGame.remove(pnlPicture);
-        pnlGame.remove(pnlAnswer);
-        showGame();
+        window.remove(pnlGame);
+        window.revalidate();
     }
     private void exitGame() {
         int option = JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?",
@@ -200,10 +193,6 @@ public class Game extends JFrame implements ActionListener {
         {
             System.exit(0);
         }
-    }
-    private void goToTitle() {
-        clearGameBoard();
-        showTitlePage();
     }
 
     //the actual game
